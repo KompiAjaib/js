da82eb76a607d6966bdb7000412f29bbe3545b47
Adhy Suryadi <KompiAjaib@users.noreply.github.com>
2020-01-04 08:34:05 +0700

    Create jsmgid.js

---
+++ jsmgid.js
@@ -0,0 +1,6 @@
+        (function () {
+        var script = document.createElement('script');
+        script.src = "//"+jsMgid+"?t=" +  ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : '');
+        script.async = true;
+        document.body.appendChild(script);
+    })();
