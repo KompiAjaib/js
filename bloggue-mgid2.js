50c9088e32c63ea59b6a64801efe88a935600e98
Adhy Suryadi <KompiAjaib@users.noreply.github.com>
2020-01-04 09:06:23 +0700

    Create bloggue-mgid2.js

---
+++ bloggue-mgid2.js
@@ -0,0 +1,6 @@
+        (function () {
+        var script = document.createElement('script');
+        script.src = "//jsc.mgid.com/b/l/bloggue.web.id.778535.js?t=" +  ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : '');
+        script.async = true;
+        document.body.appendChild(script);
+    })();
