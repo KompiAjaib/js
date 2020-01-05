e783cb5bd20d33124cb9bcb33bcde60d154cef91
Adhy Suryadi <KompiAjaib@users.noreply.github.com>
2020-01-05 15:02:40 +0700

    Create freeware-aads.js

---
+++ freeware-aads.js
@@ -0,0 +1,1 @@
+document.getElementById("aads-box").innerHTML = '<iframe data-aa="1306133" src="//acceptable.a-ads.com/1306133" scrolling="no" style="border:0px; padding:0; width: 100%; height: 60px; overflow:hidden" allowtransparency="true"></iframe>';
