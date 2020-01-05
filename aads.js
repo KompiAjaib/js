03c7505b8eb69786204a54acee17b67fa6f896c0
Adhy Suryadi <KompiAjaib@users.noreply.github.com>
2020-01-05 07:11:05 +0700

    Create aads.js

---
+++ aads.js
@@ -0,0 +1,1 @@
+document.getElementById("aads-box").innerHTML = '<iframe data-aa="997106" src="//acceptable.a-ads.com/997106" scrolling="no" style="border:0px; padding:0; width: 100%; height: 100%; overflow:hidden" allowtransparency="true"></iframe>';
